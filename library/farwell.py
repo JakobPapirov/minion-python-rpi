7755c99e521b936eeed921ea4654f3e8869d223e
Jakob Papirov <JakobPapirov@users.noreply.github.com>
2021-12-26 22:19:02 +0100

    Translated files
    Ru and Sv

--- library/farwell.py
+++ library/farwell.py
@@ -1,7 +1,7 @@
 from yachalk import chalk
 import emoji
 
-def showTotalScoreBye(rightAns, questionsNum, name):
+def showTotalScoreBye_en(rightAns, questionsNum, name):
     if rightAns == questionsNum:
       if questionsNum == 0:
         print(chalk.bg_yellow_bright(chalk.cyan("Haha, no questions wanted!")))
@@ -9,4 +9,25 @@ def showTotalScoreBye(rightAns, questionsNum, name):
         print("Wow total :) = {}".format(rightAns))
         print("Super :==) {} \o/".format(name))
         print("")
-        print("Bye" + emoji.emojize(":red_exclamation_mark:"))
+        print("Bye " + emoji.emojize(":red_exclamation_mark:"))
+
+
+def showTotalScoreBye_sv(rightAns, questionsNum, name):
+  if rightAns == questionsNum:
+    if questionsNum == 0:
+      print(chalk.bg_yellow_bright(chalk.cyan("Haha, du ville inte ha några frågor!")))
+    else:
+      print("Wow totalt :) = {}".format(rightAns))
+      print("Super :==) {} \o/".format(name))
+      print("")
+      print("Hej då " + emoji.emojize(":red_exclamation_mark:"))
+
+def showTotalScoreBye_ru(rightAns, questionsNum, name):
+  if rightAns == questionsNum:
+    if questionsNum == 0:
+      print(chalk.bg_yellow_bright(chalk.cyan("Хаха, вопросов нехотьела!")))
+    else:
+      print("Уау сумма :) = {}".format(rightAns))
+      print("Супер :==) {} \o/".format(name))
+      print("")
+      print("Пака " + emoji.emojize(":red_exclamation_mark:"))
--- library/welcomeScreen.py
+++ library/welcomeScreen.py
@@ -2,15 +2,60 @@ import random as rand
 from yachalk import chalk
 import emoji
 
-def welcome():
+def welcome_en():
 
     print("")
     name = str(input("What's your name ? "))
+    if name == '':
+        name = 'Jakob'
+    else:
+        pass
 
-    print("Welcome, " + chalk.red(chalk.bg_white("{}")).format(name) + emoji.emojize(":red_exclamation_mark:"))
+    print("Welcome, " + chalk.red(chalk.bg_white("{} ")).format(name) + emoji.emojize(":red_exclamation_mark:"))
 
     print("")
-    questionsNum = int(input("How many questions do you want ? "))
+    questionsNum = input("How many questions do you want ? ")
     print("")
+    if questionsNum == '':
+        questionsNum = 1
+    else:
+        questionsNum = int(questionsNum)
+
+    return name, questionsNum
+
+def welcome_sv():
+
+    print("")
+    name = str(input("Vad heter du ? "))
+    if name == '':
+        name = 'Jakob'
+    else:
+        pass
+
+    print("Välkommen, " + chalk.red(chalk.bg_white("{} ")).format(name) + emoji.emojize(":red_exclamation_mark:"))
+
+    print("")
+    questionsNum = input("Hur många frågor vill du ha ? ")
+    print("")
+    if questionsNum == '':
+        questionsNum = 1
+    else:
+        questionsNum = int(questionsNum)
+
+    return name, questionsNum
+
+def welcome_ru():
+
+    print("")
+    name = str(input("Как тебя завут ? "))
+    print("Привет, " + chalk.red(chalk.bg_white("{}")).format(name) + emoji.emojize(":red_exclamation_mark:"))
+
+    print("")
+    questionsNum = input("Сколько вопросов задать ? ")
+    print("")
+    if questionsNum == '':
+        questionsNum = 1
+    else:
+        questionsNum = int(questionsNum)
 
     return name, questionsNum
