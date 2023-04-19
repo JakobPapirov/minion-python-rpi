191c07f1d10a3fcdc1010d7cb150679313e97881
Jakob Papirov <JakobPapirov@users.noreply.github.com>
2023-04-19 14:39:21 +0200

    Add files via upload

---
+++ en/math-add-0-9-en.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+import random as rand # Standard library
+import emoji as emoji # pip install emoji
+from yachalk import chalk # pip install yachalk
+
+from library.welcomeScreen import welcome_en as welcome
+from library.farwell import showTotalScoreBye_en as showTotalScoreBye
+
+name, questionsNum = welcome()
+
+questionsKeeper = 1
+rightAns = 0
+progress = ''
+
+while questionsKeeper <= questionsNum:
+
+  questionsKeeper += 1
+
+  mathVarA = rand.randint(0,1)
+  mathVarB = rand.randint(0,8)
+
+  mq1In = 999999
+
+  while mq1In != (mathVarA + mathVarB):
+    mq1In = int(input("{} + {} ? ".format(mathVarA, mathVarB)))
+    # Programme fails if enter or letters are submitted
+
+    if mq1In == (mathVarA + mathVarB):
+      print(" :)")
+      print("")
+      progress = str(progress) + '='
+      print("{}".format(progress))
+      print("")
+      rightAns += 1
+      break
+    else:
+      print(" :(")
+      print("")
+
+showTotalScoreBye(rightAns, questionsNum, name)
