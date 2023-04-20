e69a48fec752319d2e9524455ec2862f60ec9917
Jakob Papirov <JakobPapirov@users.noreply.github.com>
2023-04-20 09:16:41 +0200

    Add files via upload

---
+++ en/math/lvl2/math-multi-sub-0-9-en.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+import random as rand # Standard library
+import emoji as emoji # pip install emoji
+from yachalk import chalk # pip install yachalk
+
+from library.welcomeScreen import welcome_en as welcome
+from library.farwell import showTotalScoreBye_en as showTotalScoreBye
+
+name, questionsNum = welcome()
+
+questionsKeeper = 1
+rightAns = 0
+progress = ''
+
+while questionsKeeper <= questionsNum:
+
+  questionsKeeper += 1
+
+  mathVar = 9
+
+  mathVarA = rand.randint(0,2)
+  mathVarB = rand.randint(0,2)
+  mathVarC = rand.randint(0,2)
+
+  mq1In = 999999
+
+  while mq1In != (mathVar - mathVarA - mathVarB - mathVarC):
+    mq1In = int(input("{} - {} - {} - {} ? ".format(mathVar, mathVarA, mathVarB, mathVarC)))
+    # Programme fails if enter or letters are submitted
+
+    if mq1In == (mathVar - mathVarA - mathVarB - mathVarC):
+      print(" :)")
+      print("")
+      progress = str(progress) + '='
+      print("{}".format(progress))
+      print("")
+      rightAns += 1
+      break
+    else:
+      print(" :(")
+      print("")
+
+showTotalScoreBye(rightAns, questionsNum, name)
