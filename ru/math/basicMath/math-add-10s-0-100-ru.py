d6feb5425bd9206fb4d583fd524bc564d4f330e8
Jakob Papirov <JakobPapirov@users.noreply.github.com>
2023-04-20 07:48:02 +0200

    Add files via upload

---
+++ ru/math/basicMath/math-add-10s-0-100-ru.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import random as rand # Standard library
+from yachalk import chalk # pip install yachalk
+import emoji as emoji # pip install emoji
+
+from library.welcomeScreen import welcome_en as welcome
+from library.farwell import showTotalScoreBye_en as showTotalScoreBye
+
+name, questionsNum = welcome()
+
+questionsKeeper = 1
+rightAns = 0
+progress = ''
+
+while questionsKeeper <= questionsNum:
+
+  questionsKeeper += 1
+
+  mathVarA = rand.randint(0,2) * 10
+  mathVarB = rand.randint(0,8) * 10
+
+  mq1In = 999999
+
+  while mq1In != (mathVarA + mathVarB):
+    mq1In = int(input("{} + {} ? ".format(mathVarA, mathVarB)))
+
+    if mq1In == (mathVarA + mathVarB):
+      print(" :)")
+      print("")
+      progress = str(progress) + '='
+      print("{}".format(progress))
+      print("")
+      rightAns += 1
+      break
+    else:
+      print(" :(")
+      print("")
+
+showTotalScoreBye(rightAns, questionsNum, name)
